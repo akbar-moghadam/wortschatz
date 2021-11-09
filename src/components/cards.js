f5c05a30cf333aa3de98c84b234d639bfa455c6e
akbar-moghadam <moghadam4@gmail.com>
2021-11-10 00:19:19 +0330

    initial change

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { Link } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { ReactComponent as Svg1 } from "../assets/imgs/card1.svg";
 import { ReactComponent as Svg2 } from "../assets/imgs/card2.svg";
 import { ReactComponent as Svg3 } from "../assets/imgs/card3.svg";
 const Cards = () => {
+useEffect(() => {
+  Aos.init();
+}, [])
+
   return (
     <div className="cards-container">
 
-      <div className="card1">
+      <div className="card1" data-aos="fade-right" data-aos-offset="100" data-aos-easing="ease-in-sine">
         <Link to="/a1/" className="link">
-          <Svg1 className="svg1">
+          <Svg1 className="svg1" >
 
           
           </Svg1>
         </Link>
       </div>
 
-      <div className="card2">
+      <div className="card2" data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine">
       <Link to="/a2/" className="link">
         <Svg2 className="svg2" />
         
@@ -24,7 +30,7 @@ const Cards = () => {
         </Link>
       </div>
 
-      <div className="card3">
+      <div className="card3" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine">
       <Link to="/b1/" className="link">
         <Svg3 className="svg3" />
         
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import Aos from "aos";
+import "aos/dist/aos.css";
 import '../styles/nav.scss'
 import Button from './button'
 
 const Header = () => {
+    useEffect(() => {
+      Aos.init()
+    }, [])
     return (
         <div className="header" >
             
-            <div className="header-body">
-                <div className="left">
+            <div className="header-body" data-aos="zoom-out">
+                <div className="left" >
                     <h2>Learn vocabulary without an internet connection</h2>
                     <h5>Learn vocabulary in just 5 minutes a day</h5>
                     <Button text="Start Now"/>
