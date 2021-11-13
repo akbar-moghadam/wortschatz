3e5b511b7c373df7729e1bb2d7fa012f819440a0
akbar-moghadam <moghadam4@gmail.com>
2021-11-13 23:55:00 +0330

    add login components and change css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,11 +4,19 @@ const Login = () => {
     return (
         <div className="login-container">
             <div className="login-item">
-                <h2>login item</h2>
+                <h2>Login</h2>
+                <div className="login-form">
+                <form >
+                    <label htmlFor="user">
+                    <input type="text" placeholder="Username or Email" name="user"/>
+                    </label>
+                    
+
+                </form>
             </div>
-            <div className="div">
-                login
             </div>
+           
+           
         </div>
     )
 }
